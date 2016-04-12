443c0460e9def292afc655530388b885a7e8b8e0
League <league@WTS7.attlocal.net>
2016-04-11 18:35:28 -0700

    ;lief

--- src/org/jointheleague/iaroc/Brain.java
+++ src/org/jointheleague/iaroc/Brain.java
@@ -1,5 +1,7 @@
 package org.jointheleague.iaroc;
 
+import android.os.SystemClock;
+
 import ioio.lib.api.IOIO;
 import ioio.lib.api.exception.ConnectionLostException;
 import org.wintrisstech.irobot.ioio.IRobotCreateAdapter;
@@ -21,11 +23,45 @@ public class Brain extends IRobotCreateAdapter {
     public void initialize() throws ConnectionLostException {
         dashboard.log("Hello! I'm a Clever Robot!");
         //what would you like me to do, Clever Human?
+        int totalangel =90;
+        while (totalangel> 0){
+
+            driveDirect(500,-500);
+            readSensors(SENSORS_ANGLE);ß
+            int n  = getAngle();
+           if(n>0)
+               totalangel-=n;
+            else
+               totalangel+=n;
+            dashboard.log("" +n);
+
+        }
+
+        //makesquare();
 
 
 
+        driveDirect(0,0);
+
+
+
+        dashboard.log("TEAM SMT");
 
     }
+
+    private void makesquare() throws ConnectionLostException {
+        for (int i=0;i< 4;i++) {
+
+            driveDirect(500, 500);
+            SystemClock.sleep(1000);
+            driveDirect(-500, 500);
+            SystemClock.sleep(430);
+        }
+        driveDirect(0,0);
+    }
+
     /* This method is called repeatedly. */
     public void loop() throws ConnectionLostException {}
+
+
 }
